af4909d1c75227b1289ebf02314f67bdcb163679
Kevin Agüero <kevinalbertoaguero@gmail.com>
2022-09-01 04:52:53 -0300

    prototipo de index semi terminado con caruseles

---
+++ js/sliderMenu.js
@@ -0,0 +1,18 @@
+var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 3,
+    spaceBetween: 30,
+    
+    loop: true,
+    centerSlide: 'true',
+    fade: 'true',
+    grabCursor: 'true',
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+        dynamicBullets: true,
+    },
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+});
