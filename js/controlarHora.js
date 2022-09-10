ecbe417be70818cdcf7035254f10cb36424dcfb7
De La Fuente Gonzalo <wwwgonzalito@hotmail.com>
2022-09-10 16:04:47 -0300

    se agrego la ordenacion de tabla y ordenar los pedidos poniendo los colores correspondientes que nos pidieron

---
+++ js/controlarHora.js
@@ -0,0 +1,23 @@
+function agregarHoras(fecha,horas){
+    return new Date(fecha.getTime()+ horas * 3600000);
+}
+
+var date = new Date()
+var horaActual = date.getHours() + ":" + date.getMinutes();
+var dateMasUno = agregarHoras(date,1);
+var horaActualMasUno = dateMasUno.getHours() + ":" + dateMasUno.getMinutes();
+var table = document.getElementById("tablaPedidoPendiente");
+var rows = table.rows;
+//var agregarAtributo = rows[1].getElementsByTagName("td")[0];
+// agregarAtributo.parentNode.setAttribute('class','table-danger');
+// console.log(agregarAtributo);
+for (var i = 1; i < (rows.length); i++){
+    var x = rows[i].getElementsByTagName("td")[2];
+    if(x.innerText <=  horaActual){
+        x.parentNode.setAttribute('class','table-danger');
+    }else if(( horaActual< x.innerText) && (x.innerText <= horaActualMasUno) ){
+        x.parentNode.setAttribute('class','table-warning');
+    }else if (x.innerText >horaActualMasUno){
+        x.parentNode.setAttribute('class','table-success');
+    }
+}
